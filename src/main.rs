c86bdaa454ebcde8f25ec4e70c040f67713bac50
Yuki.N <Syukkic@gmail.com>
2025-03-05 18:57:13 +0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
+use anyhow::Result;
 use config::Config;
 use db::Database;
+use feeds::UrlEntry;
 
 mod config;
 mod db;
+mod feeds;
+mod fetcher;
+mod models;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let config = Config::new()?;
-    let _ = Database::with_config(&config).await?;
+    let feeds = UrlEntry::from_file(&config)?;
+    Database::with_config(&config).await?;
 
     Ok(())
 }
